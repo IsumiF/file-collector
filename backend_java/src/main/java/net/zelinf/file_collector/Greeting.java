51a7ed1a35d574295f82607a2fc72eb7632dc23d
IsumiF <contact@zelinf.net>
2019-03-26 15:12:37 +0800

    [#3] Add empty Spring backend project

---
+++ backend_java/src/main/java/net/zelinf/file_collector/Greeting.java
@@ -0,0 +1,20 @@
+package net.zelinf.file_collector;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
