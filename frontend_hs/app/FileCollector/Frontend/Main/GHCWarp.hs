45bc45805d5ce419425fd2ff04500933225bc969
Lagrand <492680342@qq.com>
2019-04-15 14:39:02 +0800

    [#6] Add CLI options to ghc version of frontend

--- frontend_hs/app/FileCollector/Frontend/Main/GHCWarp.hs
+++ frontend_hs/app/FileCollector/Frontend/Main/GHCWarp.hs
@@ -17,11 +17,44 @@ import           Network.Wai.Application.Static         (defaultWebAppSettings,
 import qualified Network.Wai.Handler.Warp               as Warp (run)
 import           Network.WebSockets.Connection          (defaultConnectionOptions)
 import           System.FilePath                        (takeDirectory, (</>))
+import qualified Options.Applicative as Opt
+
+data Options =
+  Options
+    Int -- ^port
+    FilePath -- ^resource files base directory
 
 main :: IO ()
 main = do
     appPath <- currentPath
-    runWithDirectory 8081 (takeDirectory appPath) jsmMain
+    (Options port resourceRoot) <- Opt.execParser (optionsParserInfo (takeDirectory appPath))
+    putStrLn $ "Running on port " ++ show port
+    putStrLn $ "Resource root: " ++ resourceRoot
+    runWithDirectory port resourceRoot jsmMain
+
+optionsParser :: FilePath -> Opt.Parser Options
+optionsParser defResourceRoot = Options
+    <$> Opt.option Opt.auto
+        ( Opt.long "port"
+       <> Opt.short 'p'
+       <> Opt.metavar "PORT"
+       <> Opt.value defaultPort
+       <> Opt.showDefault
+        )
+    <*> Opt.strOption
+        ( Opt.long "resource-root"
+       <> Opt.short 'r'
+       <> Opt.metavar "RESOURCE"
+       <> Opt.value defResourceRoot
+       <> Opt.showDefaultWith id
+        )
+
+defaultPort :: Int
+defaultPort = 8081
+
+optionsParserInfo :: FilePath -> Opt.ParserInfo Options
+optionsParserInfo defResourceRoot =
+    Opt.info ((optionsParser defResourceRoot) Opt.<**> Opt.helper) Opt.fullDesc
 
 runWithDirectory :: Int -> FilePath -> JSM () -> IO ()
 runWithDirectory port dir jsm = do
