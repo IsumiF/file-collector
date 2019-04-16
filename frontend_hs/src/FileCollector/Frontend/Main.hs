a8eba83faa07efc4e93c9ebdcd5b32d22433ddb5
Lagrand <492680342@qq.com>
2019-04-16 15:59:27 +0800

    [#6] Add Bootstrap

--- frontend_hs/src/FileCollector/Frontend/Main.hs
+++ frontend_hs/src/FileCollector/Frontend/Main.hs
@@ -7,10 +7,46 @@ module FileCollector.Frontend.Main
   ) where
 
 import           Language.Javascript.JSaddle.Types (JSM)
-import           Reflex
-import           Reflex.Dom                        hiding (mainWidget)
-import           Reflex.Dom.Main                   (mainWidget)
+import           Reflex.Dom                        hiding (mainWidgetWithHead)
+import           Reflex.Dom.Main                   (mainWidgetWithHead)
 
 jsmMain :: JSM ()
-jsmMain = mainWidget $
-    text "Hello world"
+jsmMain = mainWidgetWithHead headElement bodyElement
+  
+headElement :: MonadWidget t m => m ()
+headElement = do
+    elAttr "meta" [("charset", "utf-8")] blank
+    elAttr "link"
+      [ ("rel", "shortcut icon")
+      , ("type", "image/x-icon")
+      , ("href", "static/favicon.ico")
+      ] blank
+    elAttr "meta"
+      [ ("name", "viewport")
+      , ("content", "width=device-width, initial-scale=1, shrink-to-fit=no")
+      ] blank
+    elAttr "link"
+      [ ("rel", "stylesheet")
+      , ("href", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css")
+      , ("integrity", "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T")
+      , ("crossorigin", "anonymous")
+      ] blank
+
+bodyElement :: MonadWidget t m => m ()
+bodyElement = do
+    el "p" $ text "Hello world"
+    elAttr "script"
+      [ ("src", "https://code.jquery.com/jquery-3.3.1.slim.min.js")
+      , ("integrity", "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo")
+      , ("crossorigin", "anonymous")
+      ] blank
+    elAttr "script"
+      [ ("src", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js")
+      , ("integrity", "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1")
+      , ("crossorigin", "anonymous")
+      ] blank
+    elAttr "script"
+      [ ("src", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js")
+      , ("integrity", "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM")
+      , ("crossorigin", "anonymous")
+      ] blank
