3f4099dfe0bd41dd30c85c24612aa138bc3cba4e
Isumi Feng <contact@zelinf.net>
2019-05-08 01:06:47 +0800

    Control log level in db test

--- backend/test/db/FileCollector/Backend/Database/Spec/ReadDirectorySpec.hs
+++ backend/test/db/FileCollector/Backend/Database/Spec/ReadDirectorySpec.hs
@@ -5,14 +5,16 @@ module FileCollector.Backend.Database.Spec.ReadDirectorySpec
   ( spec
   ) where
 
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Class
+import Control.Monad.Logger
+import Control.Monad.Reader
 import Data.Foldable (traverse_)
 import Data.Time (UTCTime (..), fromGregorian)
 import Database.Persist
+import Database.Persist.Sql (SqlBackend)
 import Test.Hspec
 
 import           FileCollector.Backend.Database.Impl.ReadDirectory
-    (getAllDirectories)
 import           FileCollector.Backend.Database.Spec.Util (withSqliteInMemory)
 import           FileCollector.Backend.Database.Types.Directory
 import           FileCollector.Backend.Database.Types.Role
@@ -21,15 +23,29 @@ import           FileCollector.Common.Base.Convertible
 import qualified FileCollector.Common.Types.User as Common
 
 spec :: Spec
-spec =
+spec = do
     describe "getAllDirectories" $
-      it "gives all directories in a database" $ example $ withSqliteInMemory $ do
-        isumiId <- insert $ User "isumi" "123456" (convert Common.RoleAdmin)
-        let expectedDirs =
-              [ Directory "dir1" isumiId Nothing []
-              , Directory "目录-2" isumiId (Just (UTCTime (fromGregorian 2019 5 7) 0)) []
-              ]
-        traverse_ insert_ expectedDirs
+      it "gives all directories in a database" $ example $ withSqliteInMemory LevelWarn $ do
+        expectedDirs <- insertSimpleData
 
         actualDirs <- getAllDirectories
         liftIO $ shouldMatchList actualDirs expectedDirs
+
+    describe "getDirectory" $
+      it "gives a specific directory" $ example $ withSqliteInMemory LevelWarn $ do
+        _ <- insertSimpleData
+        dirMaybe <- getDirectory "isumi" "目录-2"
+        let actualTimeMaybe = dirMaybe >>= directoryExpirationTime
+        liftIO $
+          actualTimeMaybe `shouldBe` Just (UTCTime (fromGregorian 2019 5 7) 0)
+
+insertSimpleData :: MonadIO m
+                 => ReaderT SqlBackend m [Directory]
+insertSimpleData = do
+    isumiId <- insert $ User "isumi" "123456" (convert Common.RoleAdmin)
+    let expectedDirs =
+          [ Directory "dir1" isumiId Nothing []
+          , Directory "目录-2" isumiId (Just (UTCTime (fromGregorian 2019 5 7) 0)) []
+          ]
+    traverse_ insert_ expectedDirs
+    pure expectedDirs
--- backend/test/db/FileCollector/Backend/Database/Spec/Util.hs
+++ backend/test/db/FileCollector/Backend/Database/Spec/Util.hs
@@ -1,18 +1,38 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module FileCollector.Backend.Database.Spec.Util
   ( withSqliteInMemory
+  , withSqliteConnection
   ) where
 
+import Control.Monad.IO.Unlift
 import Control.Monad.Logger
 import Control.Monad.Reader
-import Database.Persist.Sql (SqlBackend, runMigration)
+import Data.Foldable (traverse_)
+import Database.Persist.Sql (Migration, SqlBackend, runMigrationSilent)
 import Database.Persist.Sqlite (withSqliteConn)
 
-import           FileCollector.Backend.Database.Types.Internal (migrateAll)
+import FileCollector.Backend.Database.Types.Internal (migrateAll)
 
-withSqliteInMemory :: ReaderT SqlBackend (LoggingT IO) a -> IO a
-withSqliteInMemory action =
-    runStdoutLoggingT $ withSqliteConn ":memory:" (runReaderT action')
+withSqliteInMemory :: LogLevel -> ReaderT SqlBackend (LoggingT IO) a -> IO a
+withSqliteInMemory minLevel action =
+    runStdoutLoggingT . filterLoggerLevel minLevel
+      $ withSqliteConn ":memory:" (runReaderT action')
   where
-    action' = runMigration migrateAll >> action
+    action' = runMigration' migrateAll >> action
+    runMigration' = if minLevel > LevelDebug
+                    then runMigrationSilent'
+                    else runMigrationLog
+    runMigrationSilent' migration = void (runMigrationSilent migration)
+
+withSqliteConnection :: ReaderT SqlBackend (LoggingT IO) a -> SqlBackend -> IO a
+withSqliteConnection action = runStdoutLoggingT . runReaderT action
+
+filterLoggerLevel :: LogLevel -> LoggingT m a -> LoggingT m a
+filterLoggerLevel minLevel = filterLogger $ \_ level -> level >= minLevel
+
+runMigrationLog :: (MonadLogger m, MonadUnliftIO m) => Migration -> ReaderT SqlBackend m ()
+runMigrationLog migration = do
+    logs <- runMigrationSilent migration
+    traverse_ $(logDebug) logs
